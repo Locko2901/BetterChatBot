ccc426ff4462fc72ce96c851871e4ab8afca4ee5
Lockoo <simon.lockowandt@gmx.de>
2024-03-20 00:35:22 +0100

    move .zip

--- BasicSetup/BetterChatBot/ChatBot/src/conversationHistory.js
+++ BasicSetup/BetterChatBot/ChatBot/src/conversationHistory.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
 function saveConversationHistoryToFile(history) {
-  fs.writeFileSync('conversation_history.json', JSON.stringify(history), 'utf8');
+  fs.writeFileSync(path.join(__dirname, 'conversation_history.json'), JSON.stringify(history), 'utf8');
+}
+
+const path = require('path');
+
+function saveConversationHistoryToFile(history) {
+  fs.writeFileSync(path.join(__dirname, 'conversation_history.json'), JSON.stringify(history), 'utf8');
 }
 
 function loadConversationHistoryFromFile() {
   try {
-    const data = fs.readFileSync('conversation_history.json', 'utf8');
+    const data = fs.readFileSync(path.join(__dirname, 'conversation_history.json'), 'utf8');
     return JSON.parse(data);
   } catch (error) {
     return [];
